88844487dffbfda4d5ed63bfefd0c7d9b373b819
WangCute <2605735186@qq.com>
2022-11-05 14:25:29 +0800

    up

---
+++ html5_9/js/js.js
@@ -0,0 +1,45 @@
+function onName(x){
+    var temp = document.getElementById("name").value;
+    if(temp==null){
+        x.style.display = "inline";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
+
+function onPasswor(x){
+    var temp = document.getElementById("password").value;
+    if(temp==null){
+        x.style.display = "inline";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
+
+function onsex(x){
+    x.style.display = "none";
+}
+
+function onblood(x){
+    x.style.display = "none";
+}
+
+function onxg(x){
+    x.style.display = "none";
+}
+
+function onwj(x){
+    x.style.display = "none";
+}
+
+function onms(x){
+    var temp = document.getElementById("ms").value;
+    if(temp==null){
+        x.style.display = "inline";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
